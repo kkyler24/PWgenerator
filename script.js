5c4a585acc61b74797c83f8b7a118c35d70d218e
Ky Kyler <ky.kyler1@gmail.com>
2020-02-21 13:06:08 -0500

    updated JS file with Vars

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charLower = "abcdefghijklmnopqrstuvwzxy";
+var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var sym = "!@#$%^&*=_";
+
+var charNum = document.getElementById("charNum");
+var numBox = document.getElementById("numBox");
+var symbox = document.getElementById("symbox");
+var submit = document.getElementById("submit");
+var yourPw =  document.getElementById("yourPW");
 
 // Write password to the #password input
 function writePassword() {
